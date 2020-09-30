ed5311f83a282531317ad303e0b0a9eb376f939f
samirkumar111 <53811273+samirkumar111@users.noreply.github.com>
2020-09-30 15:55:09 +0530

    Create ldr_1BM18CS091.ino

---
+++ ldr_1BM18CS091.ino
@@ -0,0 +1,26 @@
+const int ledPin = 8;
+
+const int ldrPin = A0;
+
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(ledPin, OUTPUT);
+	pinMode(ldrPin, INPUT);
+}
+void loop()
+{
+	int ldrStatus = analogRead(ldrPin);
+	Serial.println(ldrStatus);
+	if(ldrStatus <=10)
+	{
+		digitalWrite(ledPin, HIGH);
+		Serial.println("LDR is DARK, LED is ON");
+	}
+	else
+	{
+		digitalWrite(ledPin, LOW);
+		Serial.println("-----------------");
+	}
+}
+	
